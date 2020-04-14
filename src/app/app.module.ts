84762ec1eb18b88206dffbb65628e3b26ae2ee92
Michael Peter <michael_piterct@hotmail.com>
2020-04-14 15:58:57 -0300

    Screen Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,

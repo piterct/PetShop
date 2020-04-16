fa93a02e2a4c65a09e8e640a3b82d0bc85b0878c
Michael Peter <michael_piterct@hotmail.com>
2020-04-16 02:35:52 -0300

    Adjusting the products grid and Currency

--- src/app/components/store/product-card/product-card.component.ts
+++ src/app/components/store/product-card/product-card.component.ts
@@ -3,7 +3,8 @@ import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-card',
-  templateUrl: './product-card.component.html'
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent implements OnInit {
   @Input()  product: Product;

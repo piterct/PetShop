1ba749221f9ecd4415d9d48586fcb165ebcd93af
Michael Peter <michael_piterct@hotmail.com>
2020-04-16 01:59:39 -0300

    Moving data for component

--- src/app/components/store/product-card/product-card.component.ts
+++ src/app/components/store/product-card/product-card.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html'
 })
 export class ProductCardComponent implements OnInit {
-
+  @Input()  product: any;
   constructor() { }
 
   ngOnInit(): void {

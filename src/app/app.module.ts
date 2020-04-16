b88a8966f04052284616bafafc65951357adad29
Michael Peter <michael_piterct@hotmail.com>
2020-04-16 01:46:43 -0300

    Create a Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './navbar/navbar.component';
+import { NavBarComponent } from './components/shared/navbar/navbar.component';
 import { LoginPageComponent } from './pages/account/login-page/login-page.component';
 import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
@@ -12,6 +12,7 @@ import { PetsPageComponent } from './pages/account/pets-page/pets-page.component
 import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 import { FramePageComponent } from './pages/master/frame-page.component';
+import { ProductCardComponent } from './components/store/product-card/product-card.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { FramePageComponent } from './pages/master/frame-page.component';
     PetsPageComponent,
     ProductsPageComponent,
     CartPageComponent,
-    FramePageComponent
+    FramePageComponent,
+    ProductCardComponent
 
   ],
   imports: [

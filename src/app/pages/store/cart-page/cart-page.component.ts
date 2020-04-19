4ed7214919a9ebae7ca6f4ab03d7f348a6b793f1
Michael Peter <michael_piterct@hotmail.com>
2020-04-19 00:03:50 -0300

    Manipulating the cart

--- src/app/pages/store/cart-page/cart-page.component.ts
+++ src/app/pages/store/cart-page/cart-page.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/models/cart.model';
+import { CartUtil } from 'src/app/utils/cart.util';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html'
 })
 export class CartPageComponent implements OnInit {
-
+  public cart: Cart = new Cart();
   constructor() { }
 
   ngOnInit(): void {
+    this.loadCart();
+  }
+
+  public loadCart() {
+    this.cart = CartUtil.get();
+  }
+
+  public remove(item) {
+    let index = this.cart.items.indexOf(item);
+    this.cart.items.splice(index, 1);
+    CartUtil.update(this.cart);
+  }
+
+  public clear(){
+    CartUtil.clear();
+    this.loadCart();
   }
 
 }

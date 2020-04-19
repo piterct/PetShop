b949d8001516602676d206419cc8f7d5a45594dd
Michael Peter <michael_piterct@hotmail.com>
2020-04-18 23:37:14 -0300

    cart util

--- src/app/models/cart-item.model.ts
+++ src/app/models/cart-item.model.ts
@@ -1,7 +1,11 @@
 export class CartItem {
-    public id: string;
-    public product: string;
-    public quantity: number;
-    public price: number;
-    public image: string;
+    constructor(
+        public id: string,
+        public product: string,
+        public quantity: number,
+        public price: number,
+        public image: string
+    ) {
+
+    }
 }
